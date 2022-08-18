ab4d1735bcc5b71f3a38c98d9eb607cb6b91d277
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-08-18 16:02:08 +0200

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,28 @@
+word_list = ["aardvark", "baboon", "camel"]
+
+import random
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+guess = input("Guess a letter: ").lower()
+
+#      Way 1
+for x in chosen_word:
+  if x == guess:
+    print("True")
+  else:
+    print("False")
+
+
+#      Way 2
+#i = 0
+#for i in range(len(chosen_word)):
+#  if guess in chosen_word[i]:
+#    print("True")
+#    i += 1
+#  else:
+#    print("False")
+#    i += 1
+  
+    
 
