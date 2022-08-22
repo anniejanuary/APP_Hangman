98ae512d400872066871404d816d8be6993e5bff
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-08-22 15:05:12 +0200

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -2,42 +2,108 @@ word_list = ["aardvark", "baboon", "camel"]
 
 import random
 
+gallows_stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 chosen_word = random.choice(word_list)
-🔎#Testing code
+#🔎Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+# setting number of lives = 6 (6 stages of gallows)
+lives = 6
 
+# turning str chosen_word into an appended list
 chosen_word_list = []
 for letter in chosen_word:
   chosen_word_list.append(letter)
 
+# creating blanks
 display = []
 for letter in chosen_word:
-  display.append("_")    🔀# display += "_"
-
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+  display.append("_")    #🔀 display += "_"
 
 end_of_game = False
 
-while not end_of_game:  🔀# while end_of_game == False:
+# while loop for guessing letters
+while not end_of_game:  #🔀 while end_of_game == False:
   guess = input("Guess a letter: ").lower()
-     
-  ❎#i = 0  -> not necessary, len(display) range starts at 0 anyway:
+  
+  # checking if guessed letter in chosen_word_list or not:
+  #❎i = 0  -> not necessary, len(display) range starts at 0 anyway:
   for i in range(len(display)):
       if chosen_word_list[i] == guess:
         display[i] = guess
-        ❎#i += 1   ->  not necessary, FOR LOOP loops w/o it
+        #❎i += 1   ->  not necessary, FOR LOOP loops w/o it
+        
+  # missed letters: deducting lives and drawing gallow stages 
+  if guess not in chosen_word_list:
+    lives -= 1    
+    print(gallows_stages[lives])
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-
-  print(display)
+    # when all lives are lost
+    if lives == 0:
+      end_of_game = True
+      print("You lose!")
+      
+  # removing commas from guessed letters display list 
+  print(' '.join(display))
   
+  # when all letters are guessed correctly
   if "_" not in display:
       end_of_game = True
-      print("You've won!")
+      print("You win!")
