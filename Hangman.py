23731d2e3c6734e59b85d5142ee0733aea606a50
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-08-22 18:41:27 +0200

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,71 +1,18 @@
-word_list = ["aardvark", "baboon", "camel"]
-
 import random
 
-gallows_stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+# using the 'word_list' from hangman_words.py
+import hangman_words
+chosen_word = random.choice(hangman_words.word_list)
 
-
-chosen_word = random.choice(word_list)
 #🔎Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
 # setting number of lives = 6 (6 stages of gallows)
 lives = 6
+
+#Importing the logo from hangman_art.py
+import hangman_ASCII_art
+print(hangman_ASCII_art.logo)
 
 # turning str chosen_word into an appended list
 chosen_word_list = []
@@ -79,29 +26,41 @@ for letter in chosen_word:
 
 end_of_game = False
 
+guessed_letters = []
+
 # while loop for guessing letters
 while not end_of_game:  #🔀 while end_of_game == False:
   guess = input("Guess a letter: ").lower()
-  
-  # checking if guessed letter in chosen_word_list or not:
-  #❎i = 0  -> not necessary, len(display) range starts at 0 anyway:
-  for i in range(len(display)):
-      if chosen_word_list[i] == guess:
-        display[i] = guess
-        #❎i += 1   ->  not necessary, FOR LOOP loops w/o it
+
+  guessed_letters += guess
+  #If the user has entered a letter they've already guessed, print the letter and let them know
+  if guess not in display:
+      
+    # checking if guessed letter in chosen_word_list or not:
+    #❎i = 0  -> not necessary, len(display) range starts at 0 anyway:
+    for i in range(len(display)):
+        if chosen_word_list[i] == guess:
+          display[i] = guess
+          #❎i += 1   ->  not necessary, FOR LOOP loops w/o it
+  else:
+    print("You've already guessed this letter, try another one: ")
         
   # missed letters: deducting lives and drawing gallow stages 
   if guess not in chosen_word_list:
+    # If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
     lives -= 1    
-    print(gallows_stages[lives])
+    print(hangman_ASCII_art.stages[lives])
+    print(f"Letter '{guess}' is not in the word")
 
     # when all lives are lost
     if lives == 0:
       end_of_game = True
       print("You lose!")
+      print(f"The word was: '{chosen_word}'")
       
-  # removing commas from guessed letters display list 
+  # turnning guessed letters display list into a str w/o commas
   print(' '.join(display))
+  print(f"Used letters: {guessed_letters}")
   
   # when all letters are guessed correctly
   if "_" not in display:
